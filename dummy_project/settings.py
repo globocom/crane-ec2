ea8bad8db93451b5fa9b5e49742c288e28cd12fe
Flavia Missi <flaviamissi@gmail.com>
2012-08-02 10:42:33 -0300

    Changed project dir name

--- dummy_project/settings.py
+++ dummy_project/settings.py
@@ -1,4 +1,4 @@
-# Django settings for crane_ec2 project.
+# Django settings for dummy_project project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'crane_ec2.urls'
+ROOT_URLCONF = 'dummy_project.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'crane_ec2.wsgi.application'
+WSGI_APPLICATION = 'dummy_project.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
