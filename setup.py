cf1b759eda55dbe8ffb0c03d3768b31494cecbc2
Francisco Souza <f@souza.cc>
2012-08-09 11:44:44 -0300

    setup: bump to 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 README = open('README.rst').read()
 
 setup(name="crane-ec2",
-      version="0.1.6",
+      version="0.1.7",
       description="helper library to service related api actions in ec2 ",
       long_description=README,
       author="timeredbull",
